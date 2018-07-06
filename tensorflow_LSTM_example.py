3acb660785426a2f2d14fda447521da59417f28a
twan <ting1234cn@163.com>
2018-07-07 07:12:14 +0800

    baseline5, keep only train function
    Signed-off-by: twan <ting1234cn@163.com>

--- tensorflow_LSTM_example.py
+++ tensorflow_LSTM_example.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
-#from tensorflow.examples.tutorials.mnist import input_data
-#mnist=input_data.read_data_sets("/tmp/data/",one_hot=True)
 from sklearn import preprocessing
 import numpy as np
 import random
@@ -9,7 +7,7 @@ import os
 from tensorflow.python import debug as tf_debug
 #define constants
 #unrolled through 28 time steps
-time_steps=15
+time_steps=25
 #hidden LSTM units
 num_units=128
 n_hidden = 128
@@ -17,11 +15,11 @@ n_hidden = 128
 n_input=13
 #learning rate for adam
 learning_rate=0.001
-#mnist is meant to be classified in 10 classes(0-9).
+#n_class is n dim output.
 n_classes=1
 #size of batch
-batch_size=15
-stock_list=["600196","600460","600276","603993","600298"]
+batch_size=25
+stock_list=["600196","600460","600276","603993"]
 stock="600276"
 #weights and biases of appropriate shape to accomplish above task
 out_weights=tf.Variable(tf.random_normal([num_units,n_classes]))
@@ -31,8 +29,6 @@ out_bias=tf.Variable(tf.random_normal([n_classes]))
 x=tf.placeholder("float",shape=[None,time_steps,n_input])
 #input label placeholder
 y=tf.placeholder("float",shape=[None,n_classes])
-writer = tf.summary.FileWriter(r'C:\Users\twan\tf', tf.get_default_graph())
-
 
 
 def gen_data(filename):
@@ -49,16 +45,14 @@ def gen_data(filename):
 
     train_y=y_hat[time_steps:len(x_seq),np.newaxis].tolist()
  #   train_y.append(label)
-
-
     return train_x, train_y
 
-feature, label = gen_data(stock+".txt")
+
 
 #processing the input tensor from [batch_size,n_steps,n_input] to "time_steps" number of [batch_size,n_input] tensors
 #input=tf.expand_dims(x,0)
 input=tf.unstack(x,axis=1)
-#input=x
+
 
 #defining the network
 #这里原文写的是n_hidden，其实不应当！应该把n_hidden改为num_units
@@ -75,47 +69,47 @@ prediction=tf.matmul(outputs[-1],out_weights)+out_bias
 loss=tf.reduce_mean(tf.square(prediction-y))
 #optimization
 opt=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-#model evaluation
-#correct_prediction=tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
-#accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 #initialize variables
 init=tf.global_variables_initializer()
 saver=tf.train.Saver()
-with tf.Session() as sess:
-  #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
-    sess.run(init)
-    iter=1
-    steps=1
-    if os.path.exists("./"+stock+"model.ckpt"):
-        saver.restore(sess,"./"+stock+"model.ckpt")
 
-    while iter < len(feature)-time_steps:
+def train(stock):
 
-        batch_x=feature[iter:iter+batch_size]
-        batch_y=label[iter:iter+batch_size]
-
-
-        sess.run(opt, feed_dict={x: batch_x, y: batch_y})
-
-        if steps % 1000 == 0:
-            #sess.run(tf.Print(y,[y],"y value is"))
-            los = sess.run(loss, feed_dict={x: batch_x, y: batch_y})
-            saver.save(sess,"./"+stock+"model.ckpt")
-
-            print("For step ", steps)
-            print("Loss ", los)
-            print("__________________")
-            if los < 0.01:
-                predict_value=sess.run(prediction,feed_dict={x: feature[len(feature)-time_steps:len(feature)], y: label[len(feature)-time_steps:len(feature)]})
-                print("prediction",predict_value[-1])
-                print("target",label[-time_steps:-1])
-
-                break
-        iter = iter + random.randint(1,time_steps)
-        steps=steps+1
-        if iter>=len(feature)-time_steps:
-            iter=1
+    feature, label = gen_data(stock+".txt")
+    with tf.Session() as sess:
+    #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
+        sess.run(init)
+        iter=1
+        steps=1
+        if os.path.exists("./"+stock+"/"+stock+"model.ckpt.meta"):
+             saver.restore(sess,"./"+stock+"/"+stock+"model.ckpt")
 
 
+        while iter < len(feature) - time_steps:
+
+            batch_x = feature[iter:iter + batch_size]
+            batch_y = label[iter:iter + batch_size]
+            sess.run(opt, feed_dict={x: batch_x, y: batch_y})
+
+            if steps % 1000 == 0:
+                # sess.run(tf.Print(y,[y],"y value is"))
+                los = sess.run(loss, feed_dict={x: batch_x, y: batch_y})
+                saver.save(sess, "./"+stock+"/"+stock+"model.ckpt")
+
+                print(stock+" for step ", steps)
+                print(stock+" Loss ", los)
+                print("__________________")
+                if los < 0.001:
+                    break
+            iter = random.randint(0, len(feature) - time_steps - 1)
+            steps = steps + 1
+
+
+
+
+if __name__ == '__main__':
+    writer = tf.summary.FileWriter(r'C:\Users\twan\tf', tf.get_default_graph())
+    for stock in stock_list:
+        train(stock)
     writer.close()
