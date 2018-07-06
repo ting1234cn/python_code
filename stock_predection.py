0a6b7617eca167ad5e13c3552c69ffe13a2d0d4b
twan <ting1234cn@163.com>
2018-07-07 07:13:26 +0800

    baseline5, seperate prediction
    Signed-off-by: twan <ting1234cn@163.com>

---
+++ stock_predection.py
@@ -0,0 +1,25 @@
+from tensorflow_LSTM_example import *
+
+def predict(stock):
+    feature, label = gen_data(stock+".txt")
+    with tf.Session() as sess:
+        #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
+        sess.run(init)
+        if os.path.exists("./"+stock+"/"+stock+"model.ckpt.meta"):
+            saver.restore(sess,"./"+stock+"/"+stock+"model.ckpt")
+        else:
+            print(stock+" model data does not exist")
+            return
+        start=len(feature)-time_steps
+        end=len(feature)
+        predict_value=sess.run(prediction,feed_dict={x: feature[start:end], y: label[start:end]})
+        print(stock+" prediction",predict_value[-1])
+        print("target",label[start:end])
+
+
+
+if __name__ == '__main__':
+    writer = tf.summary.FileWriter(r'C:\Users\twan\tf', tf.get_default_graph())
+    for stock in stock_list:
+        predict(stock)
+    writer.close()
