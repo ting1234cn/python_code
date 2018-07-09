32aa17f8721956414d5bf9ecc341c857c426c322
twan <ting1234cn@163.com>
2018-07-09 09:32:19 +0800

    update get stock data by wind support
    Signed-off-by: twan <ting1234cn@163.com>

--- Get_Stock_history_data.py
+++ Get_Stock_history_data.py
@@ -6,14 +6,16 @@ import numpy as np
 import time
 year=2017
 quater=4
-stock_list=["600196","600460","600276","603993","600298","002258"]
+stock_list=["600196","600460","600276","603993","600298","002258","002507"]
 start_date="2016-07-01"
 current_date=time.strftime("%F")
-def get_stock_hisory_tu():
+def get_stock_history_tu():
     writer = pd.ExcelWriter(r"stocks.xlsx")
     for stock in stock_list :
         if int(stock)>600000 :
             df=ts.sh_margin_details(start=start_date, end=current_date,symbol=stock)
+        else:
+            break
 
 
         df_k=ts.get_hist_data(start=start_date, end=current_date,code=stock)
@@ -28,33 +30,42 @@ def get_stock_hisory_tu():
             df_merge.to_excel(writer, stock, index=False)
     writer.save()
 
+def get_wind_data(wdata):
+    i=0
+    df = pd.DataFrame()
+    df["Date"]=wdata.Times
+    for field in wdata.Fields:
+        df[field] = wdata.Data[i]
+        i = i + 1
+
+    return df
+
+
 def get_stock_history_wind():
     writer = pd.ExcelWriter(r"stocks.xlsx")
     w.start();
+
     if w.isconnected():
         for stock in stock_list :
             if int(stock) > 600000:
-                wdata=w.wsd("002258.SZ", "open,high,low,close,volume,mrg_long_bal", start_date, current_date, "unit=1;Fill=Previous;PriceAdj=F")
-                df_k=pd.DataFrame()
-                i=0
-                for field in wdata.Fields :
-                    df_k[field]=wdata.Data[i]
-                    i=i+1
+                stock_code=stock+".SH"
+            else:
+                stock_code=stock+".SZ"
+            wdata = w.wsd(stock_code, "open,high,low,close,volume,mrg_long_bal,mrg_short_vol_bal,mfd_netbuyamt,pe_ttm,pb_lf,BOLL,MACD,roe_avg",
+                          start_date, current_date, "unit=1;traderType=1;BOLL_N=26;BOLL_Width=2;BOLL_IO=1;MACD_L=26;MACD_S=12;MACD_N=9;MACD_IO=1;MA_N=7;Fill=Previous;PriceAdj=F")
+            df_k=get_wind_data(wdata)
 
-            wdata=w.wsd("002258.SZ", "mrg_long_bal,mfd_netbuyamt,mfd_inflowrate_m,roe_yearly,ocftoor,yoyeps_basic,yoyprofit", "2017-07-01", "2018-07-06", "unit=1;Fill=Previous;traderType=1;PriceAdj=F")
-            df = pd.DataFrame()
-            i = 0
-            for field in wdata.Fields:
-                df[field] = wdata.Data[i]
-                i = i + 1
+           # wdata=w.wsd(stock_code, "mrg_long_bal,mfd_netbuyamt,mfd_inflowrate_m,roe_yearly,ocftoor,yoyeps_basic,yoyprofit",
+            #            start_date, current_date, "unit=1;Fill=Previous;traderType=1;PriceAdj=F")
+            #df = get_wind_data(wdata)
             if df_k is None:
                 break
 
 
-            df_merge=pd.merge(df,df_k,left_on='opDate',right_index=True)
-            df_merge.drop(['stockCode','securityAbbr','rqmcl','rqyl','rqchl','price_change','p_change'],axis=1,inplace=True)
-            df_merge.sort_values(by='opDate',inplace=True)
-            df_merge.to_excel(writer, stock, index=False)
+         #   df_merge=pd.merge(df_k,df,on='Date',right_index=True)
+            #df_merge.drop(['stockCode','securityAbbr','rqmcl','rqyl','rqchl','price_change','p_change'],axis=1,inplace=True)
+         #   df_merge.sort_values(by='Date',inplace=True)
+            df_k.to_excel(writer, stock, index=False)
         writer.save()
     else:
         print("wind is not connected!!")
@@ -62,4 +73,4 @@ def get_stock_history_wind():
 
 
 if __name__ == '__main__':
-    get_stock_history_wind()
+    get_stock_history_tu()
