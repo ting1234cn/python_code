8b4cdb43936cf40aabe83e141c982b21f3f945a3
twan <ting1234cn@163.com>
2018-07-12 21:58:17 +0800

    reformat code
    Signed-off-by: twan <ting1234cn@163.com>

--- stock_predection.py
+++ stock_predection.py
@@ -1,21 +1,21 @@
 from tensorflow_LSTM_example import *
 
+
 def predict(stock):
-    feature, label = gen_data(stock+".txt")
+    feature, label = gen_data(stock + ".txt")
     with tf.Session() as sess:
         #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
         sess.run(init)
-        if os.path.exists("./"+stock+"/"+stock+"model.ckpt.meta"):
-            saver.restore(sess,"./"+stock+"/"+stock+"model.ckpt")
+        if os.path.exists("./" + stock + "/" + stock + "model.ckpt.meta"):
+            saver.restore(sess, "./" + stock + "/" + stock + "model.ckpt")
         else:
-            print(stock+" model data does not exist")
+            print(stock + " model data does not exist")
             return
-        start=len(feature)-time_steps
-        end=len(feature)
-        predict_value=sess.run(prediction,feed_dict={x: feature[start:end]})
-        print(stock+" prediction",predict_value[-1])
-        #print("target",label[start:end])
-
+        start = len(feature) - time_steps
+        end = len(feature)
+        predict_value = sess.run(prediction, feed_dict={x: feature[start:end]})
+        print(stock + " prediction", predict_value[-1])
+        # print("target",label[start:end])
 
 
 if __name__ == '__main__':
--- tensorflow_LSTM_example.py
+++ tensorflow_LSTM_example.py
@@ -7,92 +7,90 @@ import os
 import time
 import pandas as pd
 from tensorflow.python import debug as tf_debug
-#define constants
-#unrolled through 28 time steps
-time_steps=30
-#hidden LSTM units
-num_units=128
-n_hidden = 128
-#rows of 28 pixels
-n_input=12
-#learning rate for adam
-learning_rate=0.001
-#n_class is n dim output.
-n_classes=2
-#size of batch
-batch_size=30
-stock_list=["600196","600460","600276","603993","600177","002507","002258"]
 
-#weights and biases of appropriate shape to accomplish above task
-out_weights=tf.Variable(tf.random_normal([num_units,n_classes]))
-out_bias=tf.Variable(tf.random_normal([n_classes]))
-#defining placeholders
-#input image placeholder
-x=tf.placeholder("float",shape=[None,time_steps,n_input])
-#input label placeholder
-y=tf.placeholder("float",shape=[None,n_classes])
+# define constants
+# unrolled through 28 time steps
+time_steps = 30
+# hidden LSTM units
+num_units = 128
+n_hidden = 128
+
+n_input = 12
+# learning rate for adam
+learning_rate = 0.001
+# n_class is n dim output.
+n_classes = 2
+# size of batch
+batch_size = 30
+stock_list = ["600196", "600460", "600276", "603993", "600177", "002507", "002258"]
+
+# weights and biases of appropriate shape to accomplish above task
+out_weights = tf.Variable(tf.random_normal([num_units, n_classes]))
+out_bias = tf.Variable(tf.random_normal([n_classes]))
+# defining placeholders
+# input image placeholder
+x = tf.placeholder("float", shape=[None, time_steps, n_input])
+# input label placeholder
+y = tf.placeholder("float", shape=[None, n_classes])
 
 
 def gen_data(filename):
     train_x, train_y = [], []
     if os.path.exists(filename):
-        dataset=np.genfromtxt(filename, dtype=float,usecols= range(1,n_input+2), skip_header=1,autostrip=True)
-        x_seq=preprocessing.scale(dataset[:,1:n_input+1])
-        #print("scaled x_seq",x_seq)
-        y_hat=dataset[:,0]
-        #print("y_hat",y_hat)
+        dataset = np.genfromtxt(filename, dtype=float, usecols=range(1, n_input + 2), skip_header=1, autostrip=True)
+        x_seq = preprocessing.scale(dataset[:, 1:n_input + 1])
+        # print("scaled x_seq",x_seq)
+        y_hat = dataset[:, 0]
+        # print("y_hat",y_hat)
         # tensorflow的输入必须array必须是n*n（2*2或者3*3），即行和列数必须一致
-        for i in range(len(x_seq)-time_steps):
-            feature=np.asarray([x_seq[i+j] for j in range(time_steps)])
+        for i in range(len(x_seq) - time_steps):
+            feature = np.asarray([x_seq[i + j] for j in range(time_steps)])
             train_x.append(feature)
-            if n_classes==0:
+            if n_classes == 0:
                 train_y = y_hat[time_steps:len(x_seq), np.newaxis].tolist()
             else:
-                label=np.asarray([y_hat[time_steps-1+i+j] for j in range(n_classes)])
+                label = np.asarray([y_hat[time_steps - 1 + i + j] for j in range(n_classes)])
                 train_y.append(label)
         return train_x, train_y
     else:
-        print(filename+" does not exist")
+        print(filename + " does not exist")
         return
 
 
+# processing the input tensor from [batch_size,n_steps,n_input] to "time_steps" number of [batch_size,n_input] tensors
+# input=tf.expand_dims(x,0)
+input = tf.unstack(x, axis=1)
 
-#processing the input tensor from [batch_size,n_steps,n_input] to "time_steps" number of [batch_size,n_input] tensors
-#input=tf.expand_dims(x,0)
-input=tf.unstack(x,axis=1)
+# defining the network
+# 这里原文写的是n_hidden，其实不应当！应该把n_hidden改为num_units
+# 或者在前面再定义一个n_hidden = 128
+
+lstm_layer = rnn.BasicLSTMCell(n_hidden, forget_bias=1)
+outputs, _ = rnn.static_rnn(lstm_layer, input, dtype="float32")
+
+# converting last output of dimension [batch_size,num_units] to [batch_size,n_classes] by out_weight multiplication
+prediction = tf.matmul(outputs[-1], out_weights) + out_bias
+
+# loss_function
+# loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y))
+loss = tf.reduce_mean(tf.square(prediction - y))
+# optimization
+opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+# initialize variables
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
 
 
-#defining the network
-#这里原文写的是n_hidden，其实不应当！应该把n_hidden改为num_units
-#或者在前面再定义一个n_hidden = 128
-
-lstm_layer=rnn.BasicLSTMCell(n_hidden,forget_bias=1)
-outputs,_=rnn.static_rnn(lstm_layer,input,dtype="float32")
-
-#converting last output of dimension [batch_size,num_units] to [batch_size,n_classes] by out_weight multiplication
-prediction=tf.matmul(outputs[-1],out_weights)+out_bias
-
-#loss_function
-#loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y))
-loss=tf.reduce_mean(tf.square(prediction-y))
-#optimization
-opt=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-
-#initialize variables
-init=tf.global_variables_initializer()
-saver=tf.train.Saver()
-
-def train(stock,new_model=False):
-
-    feature, label = gen_data(stock+".txt")
+def train(stock, new_model=False):
+    feature, label = gen_data(stock + ".txt")
     with tf.Session() as sess:
-    #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
+        #  sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
         sess.run(init)
-        iter=1
-        steps=1
-        if new_model==False and os.path.exists("./"+stock+"/"+stock+"model.ckpt.meta"):
-            saver.restore(sess,"./"+stock+"/"+stock+"model.ckpt")
-
+        iter = 1
+        steps = 1
+        if new_model == False and os.path.exists("./" + stock + "/" + stock + "model.ckpt.meta"):
+            saver.restore(sess, "./" + stock + "/" + stock + "model.ckpt")
 
         while iter <= len(feature) - time_steps:
 
@@ -103,24 +101,25 @@ def train(stock,new_model=False):
             if steps % 1000 == 0:
                 # sess.run(tf.Print(y,[y],"y value is"))
                 los = sess.run(loss, feed_dict={x: batch_x, y: batch_y})
-                saver.save(sess, "./"+stock+"/"+stock+"model.ckpt")
+                saver.save(sess, "./" + stock + "/" + stock + "model.ckpt")
                 localtime = time.asctime(time.localtime(time.time()))
-                print(localtime+"  "+stock+" for step ", steps)
-                print(stock+" Loss ", los)
+                print(localtime + "  " + stock + " for step ", steps)
+                print(stock + " Loss ", los)
                 print("__________________")
+                if los < 0.00001:
+                    break
                 if los < 0.001:
                     batch_x = feature[len(feature) - time_steps:len(feature)]
                     batch_y = label[len(feature) - time_steps:len(feature)]
-                    if sess.run(loss, feed_dict={x: batch_x, y: batch_y})<0.001:
+                    if sess.run(loss, feed_dict={x: batch_x, y: batch_y}) < 0.001:
                         break
-            iter = random.randint(0, len(feature) - time_steps)
+            if steps % 100 == 0:
+                iter = random.randint(0, len(feature) - time_steps - n_classes)
             steps = steps + 1
-
-
 
 
 if __name__ == '__main__':
     writer = tf.summary.FileWriter(r'C:\Users\twan\tf', tf.get_default_graph())
     for stock in stock_list:
-        train(stock,new_model=False)
+        train(stock, new_model=False)
     writer.close()
